686a36d8cf429f2168ec0ecdf0def951ea3bc457
MrLexotan <leite.tavares.joao@gmail.com>
2021-08-15 22:59:03 -0300

    Adicionado um pequeno slide na section time. Header fixado

---
+++ js.js
@@ -0,0 +1,14 @@
+const tempo = 3000
+
+function funcionario1() {
+    document.querySelector('.time img').src = 'img/veterinario1.png'
+    document.querySelector('.time p').innerText = 'Se um dia meu coração for consultado, para saber se andou ' +
+        'errado, será difícil negar'
+    setTimeout('funcionario2()', tempo)
+}
+
+function funcionario2() {
+    document.querySelector('.time img').src = 'img/veterinario2.jpg'
+    document.querySelector('.time p').innerText = 'meu coração tem mania de amor, amor não é fácil de achar'
+    setTimeout('funcionario1()', tempo)
+}
